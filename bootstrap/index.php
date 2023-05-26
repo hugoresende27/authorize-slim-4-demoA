2d12bb2f8e82f3470d32a4705b21d9c1cf365ceb
hugoresende27 <hugoresende27@gmail.com>
2023-05-27 00:53:54 +0100

    lesson 1 : Setup Slim

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -7,8 +8,23 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__. '/../vendor/autoload.php';
 
+$container = new Container;
 
+$settings = require __DIR__. '/../app/settings.php';
+$settings($container);
+
+
+
+// Set container to create App with on AppFactory
+AppFactory::setContainer($container);
 $app = AppFactory::create();
+
+//middleware.php
+$middleware = require __DIR__ . '/../app/middleware.php';
+$middleware($app);
+
+
+//$app->addErrorMiddleware(true,true,true);
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
